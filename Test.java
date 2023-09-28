60eead433148db8733a692d43fd902949222be86
smaniren <87126384+smaniren@users.noreply.github.com>
2023-09-28 16:27:14 +0200

    inital commit

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    /*
+     * public static void main(String[] args) {
+     * String hallo = "hallo welt 23";
+     * System.out.println(hallo);
+     * }
+     */
+}
---
+++ hihi.java
@@ -0,0 +1,7 @@
+
+public class hihi {
+    public static void main(String[] args) {
+        String test = "" + Math.round(Math.PI);
+        System.out.println(test);
+    }
+}
